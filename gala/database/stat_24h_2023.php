2f863ffbf4822cfce8a7ef2557e1858d801a23f9
dipta-sdd <dipta5160@gmail.com>
2023-06-06 09:34:50 +0600

    6-june

--- gala/database/stat_24h_2023.php
+++ gala/database/stat_24h_2023.php
@@ -5,7 +5,7 @@
     $result = $conn->query($sql);
     $data ="";
     while($row = $result->fetch_assoc()) {
-        $data.='<div class="progress m-2"><div class=" progress-bar bg-primary" role="progressbar" style="width: '.($row['score'] * 2) .'%;"aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">'. $row['player'].' '. $row['score'] .'</div></div>';
+        $data.='<div class="progress m-2"><div class=" progress-bar bg-primary" role="progressbar" style="width: '.($row['score'] ) .'%;"aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">'. $row['player'].' '. $row['score'] .'</div></div>';
     }
     $sql="select COUNT(player) as score from email2023 where player is not null and task2='true' AND `up_time`>= '2023-05-30 00:00:01';";
     $result = $conn->query($sql);
